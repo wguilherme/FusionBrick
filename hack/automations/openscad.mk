1b0c8f7fa7c1fd8ac99bfaf18f77e318f1cd9efa
wguilherme <wguilhermecriativo@gmail.com>
2026-07-05 16:56:13 -0300

    refactor: melhora a automação do openscad para geração, cores, comandos de preview e build, leitura dinâmica da pasta de parts da implementação

--- hack/automations/openscad.mk
+++ hack/automations/openscad.mk
@@ -1,17 +1,37 @@
 SHELL := /bin/bash
 OPENSCAD := openscad
-IMPL_DIR := impl/openscad
+MAGICK   := magick
+IMPL_DIR    := impl/openscad
 RENDERS_DIR := renders
-STL_DIR := $(RENDERS_DIR)/stl
-IMG_DIR := $(RENDERS_DIR)/img
-PARTS := atom plate link bridge corner
+STL_DIR     := $(RENDERS_DIR)/stl
+IMG_DIR     := $(RENDERS_DIR)/img
+PARTS       := $(basename $(notdir $(wildcard $(IMPL_DIR)/*.scad)))
 
-.PHONY: preview build $(PARTS)
+IMGSIZE     := 800,600
+COLORSCHEME := Metallic
+BG_COLOR    := \#313131
+GRAD_TOP    := \#d0d0d0
+GRAD_BOT    := \#313131
+
+.PHONY: preview build
 
 preview:
 	@mkdir -p $(IMG_DIR)
 	@for part in $(PARTS); do \
-		$(OPENSCAD) --export-format png -o $(IMG_DIR)/$$part.png $(IMPL_DIR)/$$part.scad; \
+		$(OPENSCAD) \
+			--export-format png \
+			--colorscheme=$(COLORSCHEME) \
+			--imgsize=$(IMGSIZE) \
+			--autocenter --viewall \
+			--projection=p \
+			-o $(IMG_DIR)/$$part.tmp.png \
+			$(IMPL_DIR)/$$part.scad || { echo "SKIP $$part (render failed)"; continue; }; \
+		$(MAGICK) \
+			-size 800x600 gradient:"$(GRAD_TOP)-$(GRAD_BOT)" \
+			$(IMG_DIR)/$$part.tmp.png \
+			-fuzz 5% -composite \
+			$(IMG_DIR)/$$part.png; \
+		rm -f $(IMG_DIR)/$$part.tmp.png; \
 	done
 
 build:
